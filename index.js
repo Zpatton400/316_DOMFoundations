d2534805b885fd5c75c0e0e996b556a616a3b631
Zena Patton <147436544+Zpatton400@users.noreply.github.com>
2023-12-05 23:20:30 -0500

    ALAB 316.4.1:  Form Validation
    Objectives:
    Create robust form validation using built-in HTML validation attributes and DOM event-driven JavaScript validation.

--- index.js
+++ index.js
@@ -1,82 +1,45 @@
-//Part 3:  Adding Menu Buttons
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("registration");
+  form.addEventListener("submit", function(event) {
+    const username = document.querySelector('input[name="username"]');
+    const email = document.querySelector('input[name="email"]');
+    const password = document.querySelector('input[name="password"]');
+    const passwordCheck = document.querySelector('input[name="passwordCheck"]');
+    const errorDisplay = document.getElementById("errorDisplay");
 
-// Menu data structure
-var menuLinks = [ 
-  { text: 'about', href: '/about' }, 
-  { text: 'catalog', href: '/catalog' }, 
-  { text: 'orders', href: '/orders' }, 
-  { text: 'account', href: '/account' },
-];
+    // Check if username is empty
+    if (!username.value) {
+      errorDisplay.textContent = "Username is required";
+      username.focus();
+      event.preventDefault();
+      return;
+    }
 
-// Select and cache the <main> element in a variable named mainEl
-let mainEl = document.querySelector('main');
+    // Check if email is valid
+    if (!email.validity.valid) {
+      errorDisplay.textContent = "Please enter a valid email";
+      email.focus();
+      event.preventDefault();
+      return;
+    }
 
-// Set the background color of mainEl to the value stored in the --main-bg CSS custom property
-mainEl.style.backgroundColor = 'var(--main-bg)';
+    // Check if password is empty
+    if (!password.value) {
+      errorDisplay.textContent = "Password is required";
+      password.focus();
+      event.preventDefault();
+      return;
+    }
 
-// Set the content of mainEl to <h1>DOM Manipulation</h1>
-mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+    // Check if passwords match
+    if (password.value !== passwordCheck.value) {
+      errorDisplay.textContent = "Passwords do not match";
+      passwordCheck.focus();
+      event.preventDefault();
+      return;
+    }
 
-// Add a class of flex-ctr to mainEl
-mainEl.classList.add('flex-ctr');
-
-// Select and cache the <nav id="top-menu"> element in a variable named topMenuEl
-let topMenuEl = document.querySelector('#top-menu');
-
-// Set the height of the topMenuEl element to be 100%
-topMenuEl.style.height = '100%';
-
-// Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property
-topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
-
-// Add a class of flex-around to topMenuEl
-topMenuEl.classList.add('flex-around');
-
-// Iterate over the entire menuLinks array and for each "link" object
-menuLinks.forEach(link => {
-  // Create an <a> element
-  let aElement = document.createElement('a');
-
-  // On the new element, add an href attribute with its value set to the href property of the "link" object
-  aElement.setAttribute('href', link.href);
-
-  // Set the new element's content to the value of the text property of the "link" object
-  aElement.textContent = link.text;
-
-  // Append the new element to the topMenuEl element
-  topMenuEl.appendChild(aElement);
+    // If everything is valid, clear the error message
+    errorDisplay.textContent = "";
+  });
 });
-
-import "\styles.css"
-
-// Part One:  Getting Started
-
-// Select and cache the <main> element in a variable named mainEl
-let mainEl = document.querySelector('main');
-
-// Set the background color of mainEl to the value stored in the --main-bg CSS custom property
-mainEl.style.backgroundColor = 'var(--main-bg)';
-
-// Set the content of mainEl to <h1>DOM Manipulation</h1>
-mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
-
-// Add a class of flex-ctr to mainEl
-mainEl.classList.add('flex-ctr');
-
-// Part Two:  Creating A Menu Bar 
-
-// Select and cache the <nav id="top-menu"> element in a variable named topMenuEl
-let topMenuEl = document.querySelector('#top-menu');
-
-// Set the height of the topMenuEl element to be 100%
-topMenuEl.style.height = '100%';
-
-// Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property
-topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
-
-// Add a class of flex-around to topMenuEl
-topMenuEl.classList.add('flex-around');
-
-//Part Three:  
-
-
